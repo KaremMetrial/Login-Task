43c6cc1e4dbc63301c719f79b10eb96dbc13b267
KaremMetrial <karim.metrial@gmail.com>
2026-02-03 00:14:44 +0200

    import login controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 Route::controller(LoginController::class)->group(function () {
     Route::get('/', 'show');
     Route::post('/login', 'login');
